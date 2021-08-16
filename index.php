a1a9c11b54992ef84ed517ae40f1e77b7080cf3f
rizaldyc <rizaldyc721@gmail.com>
2021-08-17 00:40:15 +0700

    Melengkapi Index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
-echo "halo";
-?>
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="ol.css" type="text/css">
+    <script src="ol.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
+
+    <script src="https://ajax.aspnetcdn.com/ajax/jQuery/jquery-3.4.1.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<h2>Judul</h2>
+<div class= "row">
+<div class= "col-lg-8">
+<div id="map" class="map" style="width: 100%; height: 550px;"></div>
+</div>
+<div class  = "col-lg-4">
+<h2>Judul</h2>
+</div>
+</div>
+</body>
+
+</html>
